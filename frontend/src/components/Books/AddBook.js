a1f423708aec5e78fb3a16be2d9376a05d4a8d58
tuan <56837376+tua2@users.noreply.github.com>
2021-11-24 14:21:02 +0700

    P2_FE ReactConcurr, REdux, Bootrap

---
+++ frontend/src/components/Books/AddBook.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const AddBook = () => {
+    return ( <
+        div className >
+        <
+        div >
+        <
+        div >
+        <
+        button type = 'button'
+        className = 'btn btn-primary'
+        data - toggle = 'modal'
+        data - target = '#exampleModal' >
+        Click to add Book
+
+
+        <
+        /button> <
+        /div> <
+        /div> <
+        /div>
+    )
+}
