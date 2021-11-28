f260a6e64b1cc7d5ce5d0911bfb3d6507458457a
tuan <56837376+tua2@users.noreply.github.com>
2021-11-29 04:00:37 +0700

    Navbar&Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddBook from './components/Books/AddBook';
-
+import Books from './components/Books/Books';
+import Navbar from './components/Navbar/Navbar';
+import RegisterUser from './components/Users/RegisterUser';
 function App() {
   return (
-    <div className="App">
-      <AddBook />
-    </div>
+   <>
+   <BrowserRouter>
+   <Navbar/>
+     <Switch> 
+       <Route exact path='/books' component={Books}/>
+         <Route exact path='/addbook' component={AddBook}/>
+         <Route exact path='/register' component={RegisterUser}/></Switch></BrowserRouter> 
+   </>
+    
   );
 }
 
--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -3,13 +3,22 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import { createBookReducer } from '../reducers/books/createBookReducer';
 import { bookListReducer } from '../reducers/books/bookListReducer';
+import { userReducer } from '../reducers/users/userAuthReducer';
 const middlewares=[thunk];
 
 const reducer=combineReducers({
     bookCreated: createBookReducer,
     booksList: bookListReducer,
+    userLogin: userReducer,
 });
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares)));
+//Get user from localstorage & save it into store
+const userAuthFromStorage=localStorage.getItem('userAuthData') ? JSON.parse(localStorage.getItem('userAuthData')): null;
+
+const initialState={
+    userLogin: {userInfo: userAuthFromStorage},
+};
+//console.log(userAuthFromStorage);
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
 
 export {store}; 
